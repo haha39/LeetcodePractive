faf6a2196d24ad8de69dc88b46fe6889ac36e38f
haha39 <60254357+haha39@users.noreply.github.com>
2023-04-05 19:46:26 +0800

    Update hash1.c

--- 1282. Group the People Given the Group Size They Belong To/hash1.c
+++ 1282. Group the People Given the Group Size They Belong To/hash1.c
@@ -1,11 +1,16 @@
-#include <>
-#include <>
-//using namespace std;
+#include <stdio.h>
+#include <stdlib.h>
+
+int** groupThePeople(int* , int , int* , int** );
 
 int main()
 {
+
 
 	system("PAUSE");
 	return 0;
 }
 
+int** groupThePeople(int* groupSizes, int groupSizesSize, int* returnSize, int** returnColumnSizes){
+
+}
