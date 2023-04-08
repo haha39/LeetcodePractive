282bd8700b6f71913ce453902e2878d5d4a46cc6
haha39 <60254357+haha39@users.noreply.github.com>
2023-04-08 23:59:09 +0800

    water

--- 1282. Group the People Given the Group Size They Belong To/sol.java
+++ 1282. Group the People Given the Group Size They Belong To/sol.java
@@ -1,12 +1,13 @@
+
 public class sol {
     public static void main(String[] args) {
 
     }
 
-    // class Solution {
-    // public List<List<Integer>> groupThePeople(int[] groupSizes) {
+    class Solution {
+        public List<List<Integer>> groupThePeople(int[] groupSizes) {
 
-    // }
-    // }
+        }
+    }
 
 }
