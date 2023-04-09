ad4cad86948429d1c2326da28e49f9a3bffc3c1e
haha39 <60254357+haha39@users.noreply.github.com>
2023-04-10 00:04:56 +0800

    Update sol.java

--- 1282. Group the People Given the Group Size They Belong To/sol.java
+++ 1282. Group the People Given the Group Size They Belong To/sol.java
@@ -11,6 +11,19 @@ public class sol {
 
             List<List<Integer>> a = new ArrayList<List<Integer>>();
 
+            List<Integer> b = new ArrayList<Integer>();
+            b.add(80);
+            List<Integer> c = new ArrayList<Integer>();
+            c.add(90);
+
+            a.add(b);
+            a.add(c);
+
+            // System.out.println(a.size());
+            // System.out.println(a.get(0).size());
+
+            System.out.println(100);
+
             return a;
 
         }
