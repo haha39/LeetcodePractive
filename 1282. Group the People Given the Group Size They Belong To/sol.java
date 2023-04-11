e690f65995dc4d6c5306bc19f2e7676456b9a96c
haha39 <60254357+haha39@users.noreply.github.com>
2023-04-11 16:30:33 +0800

    final verson

--- 1282. Group the People Given the Group Size They Belong To/sol.java
+++ 1282. Group the People Given the Group Size They Belong To/sol.java
@@ -1,5 +1,14 @@
+/*
+ * Input: groupSizes = [3,3,3,3,3,1,3]
+ *Output: [[5],[0,1,2],[3,4,6]]
+ *Explanation: 
+ *The first group is [5]. The size is 1, and groupSizes[5] = 1.
+ *The second group is [0,1,2]. The size is 3, and groupSizes[0] = groupSizes[1] = groupSizes[2] = 3.
+ *The third group is [3,4,6]. The size is 3, and groupSizes[3] = groupSizes[4] = groupSizes[6] = 3.
+ *Other possible solutions are [[2,1,6],[5],[0,4,3]] and [[5],[0,6,2],[4,3,1]].
+ */
+
 import java.util.ArrayList;
-//import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,63 +19,39 @@ public class sol {
 
         int[] groupSizes = { 3, 3, 3, 3, 3, 1, 3 };
         System.out.println(groupThePeople(groupSizes));
+
     }
 
     public static List<List<Integer>> groupThePeople(int[] groupSizes) {
 
-        List<List<Integer>> output = new ArrayList<List<Integer>>();
-        // List<Integer> a = new ArrayList<Integer>();
-        // a.add(0);
-        // a.add(1);
-        // List<Integer> b = new ArrayList<Integer>();
-        // b.add(3);
-        // output.add(a);
-        // output.add(b);
-
+        int index = 0;
         int len = groupSizes.length;
-
-        System.out.println(output.size());
-        System.out.println(groupSizes.length);
-
+        List<List<Integer>> output = new ArrayList<List<Integer>>();
         Map<Integer, Integer> groupSizesMap = new HashMap<Integer, Integer>();
+
+        // turn groupSizes into groupSizesMap(index, groupSizes[index])
         for (int i = 0; i < len; i++) {
             groupSizesMap.put(i, groupSizes[i]);
         }
 
+        // sort by value!
         List<Entry<Integer, Integer>> list = new ArrayList<>(groupSizesMap.entrySet());
         list.sort(Entry.comparingByValue());
-        list.forEach(System.out::println);
-        // groupSizesMap = sortByKeys(groupSizesMap);
+        // list.forEach(System.out::println);
 
-        for (int i = 0; i < len; i++) {
-            System.out.print(list.get(i).getKey() + "  ");
-            System.out.print(list.get(i).getValue());
-            System.out.println();
-        }
-        System.out.println();
-
-        int index = 0;
-
+        // group the people by their wanted size
         while (index < len) {
 
             List<Integer> list_tmp = new ArrayList<Integer>();
             int size = list.get(index).getValue();
 
-            System.out.println(index);
-            System.out.println(size);
-
             for (int j = 0; j < size; j++) {
                 int index_tmp = list.get(index + j).getKey();
-                System.out.print("    " + index_tmp + "    ");
-
                 list_tmp.add(index_tmp);
             }
-            System.out.println();
+
             index += size;
             output.add(list_tmp);
-            System.out.println(index);
-            System.out.println();
-
         }
 
         return output;
