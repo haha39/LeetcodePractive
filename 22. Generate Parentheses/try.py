68e341c179b07885e8cfe0e52d99c208df7f3030
haha39 <60254357+haha39@users.noreply.github.com>
2023-07-03 14:47:09 +0800

    Update try.py

--- 22. Generate Parentheses/try.py
+++ 22. Generate Parentheses/try.py
@@ -5,19 +5,18 @@ class DP(object):
     def Fibonacci(self, n):
 
         # np
-        np1 = np.zeros(10)
+        np1 = np.zeros(n)
 
         print(np1)
         np1[0] = 1
         np1[1] = 1
 
-        for i in range(2, 10, 1):
+        for i in range(2, n, 1):
             np1[i] = np1[i-1] + np1[i-2]
 
         print(np1)
 
 
 if __name__ == '__main__':
-    print("9")
     haha = DP()
-    haha.Fibonacci(1)
+    haha.Fibonacci(10)
