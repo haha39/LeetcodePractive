ef3cdac2e3fd6842a3c3cc4b37eb4d98f498ee8b
haha39 <60254357+haha39@users.noreply.github.com>
2023-07-03 13:32:24 +0800

    Update try.py

--- 22. Generate Parentheses/try.py
+++ 22. Generate Parentheses/try.py
@@ -1,6 +1,13 @@
-class Solution(object):
-    def generateParenthesis(self, n):
+class DP(object):
+    def Fibonacci(self, n):
         """
         :type n: int
         :rtype: List[str]
         """
+        print(n)
+
+
+if __name__ == '__main__':
+    print("9")
+    haha = DP()
+    haha.Fibonacci(1)
