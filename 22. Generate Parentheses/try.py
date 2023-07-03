2b4bccabd3cba1aa66445fd2ef89c9e1b970f09d
haha39 <60254357+haha39@users.noreply.github.com>
2023-07-03 14:45:00 +0800

    Update try.py

--- 22. Generate Parentheses/try.py
+++ 22. Generate Parentheses/try.py
@@ -1,10 +1,20 @@
+import numpy as np
+
+
 class DP(object):
     def Fibonacci(self, n):
-        """
-        :type n: int
-        :rtype: List[str]
-        """
-        print(n)
+
+        # np
+        np1 = np.zeros(10)
+
+        print(np1)
+        np1[0] = 1
+        np1[1] = 1
+
+        for i in range(2, 10, 1):
+            np1[i] = np1[i-1] + np1[i-2]
+
+        print(np1)
 
 
 if __name__ == '__main__':
