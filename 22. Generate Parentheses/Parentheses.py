c309c6239842c32e6d285aef330e1de42856eebc
haha39 <60254357+haha39@users.noreply.github.com>
2023-07-06 14:14:19 +0800

    Update Parentheses.py

--- 22. Generate Parentheses/Parentheses.py
+++ 22. Generate Parentheses/Parentheses.py
@@ -7,37 +7,53 @@ class Solution(object):
         :type n: int
         :rtype: List[str]
         """
-        words = ["()()", "(())", "()()"]
+        # words = ["()()", "(())", "()()"]
 
-        a = list(set(words))
-        b = np.unique(words)
-        c = list(dict.fromkeys(words))
-        d = []
-        [d.append(x) for x in words if x not in d]
+        # a = list(set(words))
+        # b = np.unique(words)
+        # c = list(dict.fromkeys(words))
+        # d = []
+        # [d.append(x) for x in words if x not in d]
 
-        print(a)
-        print(b)
-        print(c)
-        print(d)
+        # # print(a)
+        # # print(b)
+        # # print(c)
+        # # print(d)
 
-        len1 = len(words)
-        print(len1)
+        # # len1 = len(words)
+        # # print(len1)
 
-        list1 = []
+        result = []
 
-        list1.append(["()"])
-        list1.append(["()"])
-        list1.append(["()"])
-        print(list1)
-        print(list1[0])
+        result.append(["()"])
+        # # print(result)
 
-        # for i in range(1, n, 1):
-        #     print(i)
+        for i in range(1, n, 1):
+            # print(i)
+            tmp = []
+            length = len(result[i-1])
+            # print(length)
+
+            for j in range(length):
+                str1 = "()" + result[i-1][j]
+                str2 = "(" + result[i-1][j] + ")"
+                str3 = result[i-1][j] + "()"
+                tmp.append(str1)
+                tmp.append(str2)
+                tmp.append(str3)
+
+            # print(tmp)
+            result.append(list(set(tmp)))
+            # print(result)
+
+        return result[n-1]
 
 
 if __name__ == '__main__':
     haha = Solution()
-    haha.generateParenthesis(3)
+    ya = haha.generateParenthesis(4)
+
+    print(ya)
 
 
 '''
