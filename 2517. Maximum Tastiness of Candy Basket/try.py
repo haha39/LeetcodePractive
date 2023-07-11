21a8d10b6537613884537d46e068a4cbb7f13303
haha39 <60254357+haha39@users.noreply.github.com>
2023-07-11 11:22:18 +0800

    Update try.py

--- 2517. Maximum Tastiness of Candy Basket/try.py
+++ 2517. Maximum Tastiness of Candy Basket/try.py
@@ -1,7 +1,14 @@
-class Solution(object):
+class BS(object):
     def maximumTastiness(self, price, k):
         """
         :type price: List[int]
         :type k: int
         :rtype: int
         """
+
+
+if __name__ == '__main__':
+
+    x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    haha = BS()
+    haha.maximumTastiness(10)
