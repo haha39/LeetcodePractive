59f113a26fb59ee1dbe07c9c9f693f4ba50533ba
haha39 <60254357+haha39@users.noreply.github.com>
2023-07-11 11:30:47 +0800

    Update try.py

--- 2517. Maximum Tastiness of Candy Basket/try.py
+++ 2517. Maximum Tastiness of Candy Basket/try.py
@@ -5,10 +5,30 @@ class BS(object):
         :type k: int
         :rtype: int
         """
+        left, right = price[0], price[-1]
+        mid = int((left + right) / 2)
+        print(left, right, mid)
+
+        while (True):
+            if left == k:
+                print(0)
+                break
+            elif right == k:
+                print(1)
+                break
+            elif mid == k:
+                print(2)
+                break
+            elif k > mid:
+                left = mid
+                mid = int((mid + right) / 2)
+            elif k < mid:
+                right = mid
+                mid = int((mid + left) / 2)
 
 
 if __name__ == '__main__':
 
     x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     haha = BS()
-    haha.maximumTastiness(10)
+    haha.maximumTastiness(x, 2)
