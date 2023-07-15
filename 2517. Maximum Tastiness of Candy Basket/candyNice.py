8fcbd4df6a83de876daf7653bbd863344434cd27
haha39 <60254357+haha39@users.noreply.github.com>
2023-07-15 16:51:33 +0800

    Create candyNice.py

---
+++ 2517. Maximum Tastiness of Candy Basket/candyNice.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def maximumTastiness(self, price, k):
+        """
+        :type price: List[int]
+        :type k: int
+        :rtype: int
+        """
+
+
+if __name__ == '__main__':
+
+    x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    haha = Solution()
+    haha.maximumTastiness(x, 2)
