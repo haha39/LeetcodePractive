174cede2023a7d4df7eac00921c5689a453040d0
haha39 <60254357+haha39@users.noreply.github.com>
2023-07-19 19:08:53 +0800

    Update candyNice.py

--- 2517. Maximum Tastiness of Candy Basket/candyNice.py
+++ 2517. Maximum Tastiness of Candy Basket/candyNice.py
@@ -1,3 +1,11 @@
+'''
+in my mind : 
+we need two part
+partA is BS, the goal is to find the needed number
+partB is to check whether the number can fit the tasteness of k candies or not
+'''
+
+
 class Solution(object):
     def maximumTastiness(self, price, k):
         """
@@ -5,17 +13,32 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
+
+        def BS_check(self, price, k, temp_min):
+            last, ct, i = price[0], 1, 1
+
+            while ct < k and i < len(price):
+                if price[i] - last >= temp_min:
+                    last = price[i]
+                    ct += 1
+
+            return (ct == k)
+
         lo, hi = 0, 10 ** 9
         price.sort()
 
-        return 0
+        while lo < hi:
+            mid = lo + (hi - lo) // 2
+            if BS_check(mid):
+                lo = mid + 1
+            else:
+                hi = mid
 
-    def check(self, price, temp_min):
-        last, ct, i = price[0], 1, 1
+        return lo - 1
 
 
 if __name__ == '__main__':
 
-    x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    x = [13, 5, 1, 8, 21, 2]
     haha = Solution()
-    haha.maximumTastiness(x, 2)
+    haha.maximumTastiness(x, 3)
