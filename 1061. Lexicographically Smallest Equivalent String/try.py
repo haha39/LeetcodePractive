ef8420051725c6823b5a75ace9f8da59e72798fe
haha39 <60254357+haha39@users.noreply.github.com>
2023-07-24 18:36:16 +0800

    Update try.py

--- 1061. Lexicographically Smallest Equivalent String/try.py
+++ 1061. Lexicographically Smallest Equivalent String/try.py
@@ -1,3 +1,9 @@
+class Node:
+    def __init__(self):
+        self.parent = self
+        self.rank = 0
+
+
 class Solution(object):
     def smallestEquivalentString(self, s1, s2, baseStr):
         """
@@ -6,6 +12,27 @@ class Solution(object):
         :type baseStr: str
         :rtype: str
         """
+        length = len(s1)
+        str = ""
+
+        for i in range(length):
+            Solution.union(s1[i], s2[i])
+
+    def find(self, element):
+        return self._find(self.sets[element])
+
+    def _find(self, n):
+        if n.parent != n:
+            # path compression
+            n.parent = self._find(n.parent)
+        return n.parent
+
+    def union(self, u, v):
+        u = self.find(u)
+        v = self.find(v)
+        if u != v:
+            self.parents[u] = v
+            self.count -= 1
 
 
 if __name__ == '__main__':
