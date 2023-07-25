0ec30bb8ec52c9a9f5656db0a91b81ad592b3803
haha39 <60254357+haha39@users.noreply.github.com>
2023-07-25 18:17:40 +0800

    Update sol.py

--- 1061. Lexicographically Smallest Equivalent String/sol.py
+++ 1061. Lexicographically Smallest Equivalent String/sol.py
@@ -1,6 +1,3 @@
-list1 = []
-for i in range(26):
-    list1.append(i)
 
 
 class Solution(object):
@@ -12,46 +9,54 @@ class Solution(object):
         :type baseStr: str
         :rtype: str
         """
+        list1 = []
+        for i in range(26):
+            list1.append(i)
+
         len1 = len(s1)
         len2 = len(baseStr)
         str = ""
+        #
+
+        def find(node):
+            if node != list1[node]:
+                list1[node] = find(list1[node])
+
+            return list1[node]
+
+        def union(u, v):
+            root_u = find(u)
+            root_v = find(v)
+            # print(root_u)
+            # print(root_v)
+
+            if root_u < root_v:
+                list1[root_v] = root_u
+            elif root_u > root_v:
+                list1[root_u] = root_v
 
         # union
         for i in range(len1):
             print(i)
             index1, index2 = ord(s1[i]) - 97, ord(s2[i]) - 97
-            Solution.union(index1, index2)
+            union(index1, index2)
 
-            print(list1)
+            # print(list1)
 
         # find
         for i in range(len2):
             num = ord(baseStr[i]) - 97
-            num = Solution.find(num)
+            num = find(num)
             num = num + 97
             str = str + chr(num)
 
-        print(str)
-
-    def find(node):
-        if node != list1[node]:
-            list1[node] = Solution.find(list1[node])
-
-        return list1[node]
-
-    def union(u, v):
-        root_u = Solution.find(u)
-        root_v = Solution.find(v)
-
-        if root_u < root_v:
-            list1[v] = root_u
-        elif root_u > root_v:
-            list1[u] = root_v
+        # print(str)
+        return str
 
 
 if __name__ == '__main__':
-    s1 = "parker"
-    s2 = "morris"
-    baseStr = "parser"
+    s1 = "leetcode"
+    s2 = "programs"
+    baseStr = "sourcecode"
     haha = Solution()
     haha.smallestEquivalentString(s1, s2, baseStr)
