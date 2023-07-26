b4da70df71519bb555ca651668eb49ef583e30b3
haha39 <60254357+haha39@users.noreply.github.com>
2023-07-26 22:37:24 +0800

    final version of solution

--- 1061. Lexicographically Smallest Equivalent String/sol.py
+++ 1061. Lexicographically Smallest Equivalent String/sol.py
@@ -1,5 +1,3 @@
-
-
 class Solution(object):
 
     def smallestEquivalentString(self, s1, s2, baseStr):
@@ -9,6 +7,7 @@ class Solution(object):
         :type baseStr: str
         :rtype: str
         """
+        # initial value
         list1 = []
         for i in range(26):
             list1.append(i)
@@ -16,34 +15,31 @@ class Solution(object):
         len1 = len(s1)
         len2 = len(baseStr)
         str = ""
-        #
 
+        # find with compression
         def find(node):
             if node != list1[node]:
                 list1[node] = find(list1[node])
 
             return list1[node]
 
+        # union by ascii
         def union(u, v):
             root_u = find(u)
             root_v = find(v)
-            # print(root_u)
-            # print(root_v)
 
             if root_u < root_v:
                 list1[root_v] = root_u
             elif root_u > root_v:
                 list1[root_u] = root_v
 
-        # union
+        # union s1 and s2
         for i in range(len1):
-            print(i)
+            # print(i)
             index1, index2 = ord(s1[i]) - 97, ord(s2[i]) - 97
             union(index1, index2)
 
-            # print(list1)
-
-        # find
+        # find by baseStr
         for i in range(len2):
             num = ord(baseStr[i]) - 97
             num = find(num)
@@ -55,8 +51,10 @@ class Solution(object):
 
 
 if __name__ == '__main__':
+
     s1 = "leetcode"
     s2 = "programs"
     baseStr = "sourcecode"
+
     haha = Solution()
     haha.smallestEquivalentString(s1, s2, baseStr)
