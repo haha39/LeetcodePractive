6b1ec0426558054c44dfd3006a4e7824af3636ce
haha39 <60254357+haha39@users.noreply.github.com>
2023-07-30 23:37:35 +0800

    Update try.py

---
+++ 841. Keys and Rooms/try.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def canVisitAllRooms(self, rooms):
+        """
+        :type rooms: List[List[int]]
+        :rtype: bool
+        """
+
+
+if __name__ == '__main__':
+    haha = Solution()
+    haha.canVisitAllRooms(10)
