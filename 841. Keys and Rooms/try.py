a48e0f34a67b97f6dacbabf8e7c93f02ea9b29ae
haha39 <60254357+haha39@users.noreply.github.com>
2023-08-08 20:40:20 +0800

    Update try.py

--- 841. Keys and Rooms/try.py
+++ 841. Keys and Rooms/try.py
@@ -1,34 +1,34 @@
 class Solution(object):
     def canVisitAllRooms(self, rooms):
+        def dfs(graph, start):
+            stack = []
+            result = []
+
+            stack.append(start)
+            visited = set()
+            visited.add(start)
+
+            while (len(stack) > 0):
+                currentVertex = stack.pop()
+                result.append(currentVertex)
+
+                for neighbor in graph[currentVertex]:
+                    if neighbor not in visited:
+
+                        stack.append(neighbor)
+                        visited.add(neighbor)
+
+            return len(result) == len(graph)
+
         """
         :type rooms: List[List[int]]
         :rtype: bool
         """
         self.rooms = rooms
 
-        output = Solution.dfs(self.rooms, 0)
+        output = dfs(self.rooms, 0)
 
         return output
-
-    def dfs(graph, start):
-        stack = []
-        result = []
-
-        stack.append(start)
-        visited = set()
-        visited.add(start)
-
-        while (len(stack) > 0):
-            currentVertex = stack.pop()
-            result.append(currentVertex)
-
-            for neighbor in graph[currentVertex]:
-                if neighbor not in visited:
-
-                    stack.append(neighbor)
-                    visited.add(neighbor)
-
-        return len(result) == len(graph)
 
 
 if __name__ == '__main__':
