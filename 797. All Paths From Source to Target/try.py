ebf0f24bd31437038f15a5eb10c93943194fe942
haha39 <60254357+haha39@users.noreply.github.com>
2023-08-12 18:56:58 +0800

    Update try.py

--- 797. All Paths From Source to Target/try.py
+++ 797. All Paths From Source to Target/try.py
@@ -3,17 +3,21 @@ class Solution(object):
         def bfs(graph, start):
 
             queue = []
-            queue.append(start)
             result = []
             visited = set()
+
+            queue.append(start)
             visited.add(start)
+
             while (len(queue) > 0):
                 currentVertex = queue.pop(0)
                 result.append(currentVertex)
+
                 for neighbor in graph[currentVertex]:
                     if neighbor not in visited:
                         queue.append(neighbor)
                         visited.add(neighbor)
+
             return result
 
         """
@@ -24,8 +28,8 @@ class Solution(object):
 
 if __name__ == '__main__':
     haha = Solution()
-    rooms = [[1], [2], [3], []]
+    graph = [[4, 3, 1], [3, 2, 4], [3], [4], []]
 
-    fish = haha.allPathsSourceTarget(rooms)
+    fish = haha.allPathsSourceTarget(graph)
 
     print(fish)
