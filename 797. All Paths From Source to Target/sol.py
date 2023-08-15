4acc2883052aeca652f9b362ddc4ef4cdda597eb
haha39 <60254357+haha39@users.noreply.github.com>
2023-08-15 17:05:03 +0800

    Update sol.py

---
+++ 797. All Paths From Source to Target/sol.py
@@ -0,0 +1,28 @@
+import copy
+
+
+class Solution(object):
+    def allPathsSourceTarget(self, graph):
+
+        def bfs(rev_dir_graph, start, end, path):
+            u = 1
+
+        x = 1
+        return 0
+
+
+if __name__ == '__main__':
+
+    haha = Solution()
+
+    graph0 = [[1, 2], [3], [3], []]
+
+    graph1 = [[4, 3, 1], [3, 2, 4], [3], [4], []]
+
+    graph2 = [[3, 1], [4, 6, 7, 2, 5], [4, 6, 3],
+              [6, 4], [7, 6, 5], [6], [7], []]
+
+    jojo = haha.allPathsSourceTarget(graph2)
+
+    print("answer : \n\n")
+    print(jojo)
