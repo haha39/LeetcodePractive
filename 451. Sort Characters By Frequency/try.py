ff0365007e5678a998c83f15228faa636232737d
haha39 <60254357+haha39@users.noreply.github.com>
2023-08-18 20:01:44 +0800

    Update try.py

---
+++ 451. Sort Characters By Frequency/try.py
@@ -0,0 +1,19 @@
+
+class Solution(object):
+    def frequencySort(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+
+
+if __name__ == '__main__':
+    haha = Solution()
+    str1 = "tree"
+
+    str2 = "cccaaa"
+
+    yeha = haha.allPathsSourceTarget(str1)
+
+    print("answer : \n\n")
+    print(yeha)
