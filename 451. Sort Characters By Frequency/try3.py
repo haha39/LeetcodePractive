a0c71b5e0997888c4994553bc7cbaabe10863ffc
haha39 <60254357+haha39@users.noreply.github.com>
2023-08-24 20:14:56 +0800

    Update try3.py

---
+++ 451. Sort Characters By Frequency/try3.py
@@ -0,0 +1,46 @@
+'''
+heap / priority queue
+'''
+
+
+class Solution(object):
+    def frequencySort(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        dict = {}
+        size = len(s)
+        output = ""
+
+        for i in range(size):
+            if s[i] in dict:
+                ct = dict.get(s[i])
+                ct = ct+1
+                dict.update({s[i]: ct})
+            else:
+                dict.update({s[i]: 1})
+
+        # print(dict)
+
+        sorted_dict = sorted(dict.items(), key=lambda x: x[1], reverse=True)
+
+        # print(sorted_dict)
+
+        for key, value in sorted_dict:
+            for i in range(value):
+                output = output + key
+
+        return output
+
+
+if __name__ == '__main__':
+    haha = Solution()
+    str1 = "tree"
+
+    str2 = "cccaaa"
+
+    yeha = haha.frequencySort(str1)
+
+    print("answer : \n\n")
+    print(yeha)
