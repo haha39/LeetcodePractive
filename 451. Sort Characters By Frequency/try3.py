c415dcd594117b2ec99d09b437de9b22e2e1f74b
haha39 <60254357+haha39@users.noreply.github.com>
2023-08-25 14:07:48 +0800

    Update try3.py

--- 451. Sort Characters By Frequency/try3.py
+++ 451. Sort Characters By Frequency/try3.py
@@ -3,6 +3,9 @@ heap / priority queue
 '''
 
 
+from heapq import heappush
+
+
 class Solution(object):
     def frequencySort(self, s):
         """
@@ -16,19 +19,27 @@ class Solution(object):
         for i in range(size):
             if s[i] in dict:
                 ct = dict.get(s[i])
-                ct = ct+1
+                ct = ct - 1
                 dict.update({s[i]: ct})
             else:
-                dict.update({s[i]: 1})
+                dict.update({s[i]: -1})
 
         print(dict)
+
+        pq = []
+        for key, value in dict.items():
+            heappush(pq, (value, key))
+
+        print(pq)
+
+        len_pq = len(pq)
 
         return output
 
 
 if __name__ == '__main__':
     haha = Solution()
-    str1 = "tree"
+    str1 = "cbaa"
 
     str2 = "cccaaa"
 
