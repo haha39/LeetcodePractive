83659e581cc042af60142309b06cdfe55a38d870
haha39 <60254357+haha39@users.noreply.github.com>
2023-08-29 18:26:05 +0800

    Update try.py

---
+++ 1203. Sort Items by Groups Respecting Dependencies/try.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def sortItems(self, n, m, group, beforeItems):
+        """
+        :type n: int
+        :type m: int
+        :type group: List[int]
+        :type beforeItems: List[List[int]]
+        :rtype: List[int]
+        """
+
+
+if __name__ == '__main__':
+    haha = Solution()
+    n = 8
+    m = 2
+    group = [-1, -1, 1, 0, 0, 1, 0, -1]
+    beforeItems = [[], [6], [5], [6], [3, 6], [], [], []]
+
+    nas = haha.sortItems(n, m, group, beforeItems)
+
+    print(nas)
